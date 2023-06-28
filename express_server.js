89db9339a794b258512552c85503b1f759dc2d8a
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-28 03:29:13 -0400

    Add POST route for form submission

--- express_server.js
+++ express_server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
@@ -16,6 +17,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
