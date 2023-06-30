0106956fb90ab7c1b6ca560c57561209424c0c1d
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-29 21:11:19 -0400

    Display username via cookie-parser and reset header/clear cookies on logout

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -23,12 +25,8 @@ app.set("view engine", "ejs");
 // Middleware for parsing POST request body to make it human-readable:
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -40,11 +38,12 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -74,6 +73,11 @@ app.post("/urls/:id/edit", (req, res) => {
 app.post("/login", (req, res) => {
   const name = req.body.username;
   res.cookie("username", name);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
